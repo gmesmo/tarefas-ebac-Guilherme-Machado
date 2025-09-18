2527e8e3eb6aa606d84400b5485dd9015907be7e
GMesmo <guimachado94@gmail.com>
2025-09-18 14:33:37 -0300

    Execucao do excericio mod8

---
+++ mod8/src/CalculoMedia.java
@@ -0,0 +1,11 @@
+public class CalculoMedia {
+    public static void main(String[] args) {
+        double nota1 = 9;
+        double nota2 = 8.5;
+        double nota3 = 7;
+        double nota4 = 10;
+
+        double media = (nota1 + nota2 + nota3 + nota4) / 4;
+        System.out.println(media);
+    }
+}
