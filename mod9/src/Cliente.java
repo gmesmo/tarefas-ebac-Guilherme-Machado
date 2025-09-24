ab8bd1e333d0c968e4d68556f40a50621c577118
GMesmo <guimachado94@gmail.com>
2025-09-24 16:26:06 -0300

    implementação do exercicio do mod9

--- mod9/src/Cliente.java
+++ mod9/src/Cliente.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Cliente {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Informe o código do cliente (número inteiro): ");
+    int numero = scanner.nextInt();
+
+    Integer numeroWrapper = numero;
+    System.out.println("O código digitado foi "+numeroWrapper);
+
+    scanner.close();
+    }
 }
