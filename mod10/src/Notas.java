0604c04e9fb23b3c676c3b9d084bd8ec1405c223
GMesmo <guimachado94@gmail.com>
2025-11-17 11:28:22 -0300

    implementação do exercício

--- mod10/src/Notas.java
+++ mod10/src/Notas.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Notas {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double[] notas = new double[4];
+        double media = 0;
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println("Digite a nota " + (i+1) + ": ");
+            notas[i] = scanner.nextDouble();
+            media = media + notas[i];
+        }
+
+        media = media / 4;
+
+        if (media >= 7) {
+            System.out.println("Aprovado!!");
+        } else if (media >= 5) {
+            System.out.println("Recuperação!");
+        } else {
+            System.out.println("Reprovado.");
+        }
+
+        scanner.close();
+    }
 }
