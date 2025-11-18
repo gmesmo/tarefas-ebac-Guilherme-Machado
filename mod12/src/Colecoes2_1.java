b4f594b18a431c5564813b225851de04188152a0
GMesmo <guimachado94@gmail.com>
2025-11-18 16:34:47 -0300

    exercício mod 12

--- mod12/src/Colecoes2_1.java
+++ mod12/src/Colecoes2_1.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.util.*;
 
 public class Colecoes2_1 {
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String pessoas;
+
+        System.out.println("Informe as pessoas separadas por vírgula: ");
+        pessoas = scanner.nextLine();
+
+        List<String> listaPessoas = new ArrayList<>();
+
+        String[] ArrayPessoas = pessoas.split(",");
+
+        for (int i = 0; i < ArrayPessoas.length; i++) {
+            listaPessoas.add(ArrayPessoas[i]);
+        }
+
+        Collections.sort(listaPessoas);
+
+        System.out.println(listaPessoas);
+    }
 }
